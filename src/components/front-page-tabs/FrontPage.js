9cf37055b1d135b159e28ab5ba1c5b7ad4dccdab
Vitor <vitor93gs@gmail.com>
2022-06-07 17:52:38 -0300

    criadas animações da pagina principal

--- src/components/front-page-tabs/FrontPage.js
+++ src/components/front-page-tabs/FrontPage.js
@@ -1,6 +1,5 @@
 import Image from "next/dist/client/image";
 import logoBig from "../../assets/logoBig.png";
-import { Transition } from "@headlessui/react";
 import Typewriter from "typewriter-effect";
 
 export function FrontPage() {
@@ -8,34 +7,29 @@ export function FrontPage() {
     <div className="bg-background bg-center bg-no-repeat bg-cover flex justify-center items-center h-screen pr-36 pl-36">
       <Image src={logoBig} alt="Logo smart Business" width="940" height="620" />
       <div className="flex flex-col items-center justify-between w-1/2 text-left">
-        <h1 className="text-gray1 text-4xl text-opacity-70 text-right mb-12">
+        <h1 className="text-gray1 text-4xl text-opacity-70 text-right mb-12 h-36">
           <Typewriter
             onInit={(typewriter) => {
               typewriter
+                .changeDelay(40)
                 .typeString("BAIXE NOSSOS EBOOKS!")
                 .pauseFor(2500)
                 .deleteAll()
                 .typeString("Criptoeconomia ( Conceitos e Curiosidades )")
+                .pauseFor(2500)
                 .start();
             }}
+            options={{ loop: true }}
           />
         </h1>
 
-        <Transition
-          appear={true}
-          show={true}
-          enter="transition ease-out duration-1000"
-          enterFrom="opacity-0 translate-y-full"
-          enterTo="opacity-100 translate-y-0"
-          leave="transition ease-in duration-300"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0 scale-90"
-          className="w-2/5"
-        >
-          <button className=" text-golden opacity-75  border-2   shadow-golden border-golden ">
-            SAIBA MAIS
-          </button>
-        </Transition>
+        <a className="div-neon opacity-60 rounded-sm text-center">
+          <span className="child1"></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          SAIBA MAIS
+        </a>
       </div>
     </div>
   );
