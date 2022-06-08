ea0df88bf58a6f1a27db060593c1a239975ff002
Vitor <vitor93gs@gmail.com>
2022-06-08 14:00:30 -0300

    retirado router com useeffect para testar sem router

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,9 @@ import { useState } from "react";
 import { Navbar } from "../components/Navbar";
 import Tabs from "../components/Tabs";
 import { Loading } from "../components/Loading";
-import NProgress from "nprogress";
-import Router from "next/router";
-import { useEffect } from "react";
+// import NProgress from "nprogress";
+// import Router from "next/router";
+// import { useEffect } from "react";
 
 export default function Home() {
   setTimeout(() => {
@@ -14,27 +14,27 @@ export default function Home() {
 
   const [isLoading, setLoading] = useState(true);
 
-  const handleStart = () => {
-    setLoading(true);
-  };
-  const handleComplete = () => {
-    setLoading(false);
-  };
+  // const handleStart = () => {
+  //   setLoading(true);
+  // };
+  // const handleComplete = () => {
+  //   setLoading(false);
+  // };
 
-  useEffect(() => {
-    Router.events.on("routeChangeStart", () => {
-      NProgress.start();
-      handleStart();
-    });
-    Router.events.on("routeChangeComplete", () => {
-      handleComplete();
-      NProgress.done();
-    });
-    Router.events.on("routeChangeError", () => {
-      handleComplete();
-      NProgress.done();
-    });
-  }, []);
+  // useEffect(() => {
+  //   Router.events.on("routeChangeStart", () => {
+  //     NProgress.start();
+  //     handleStart();
+  //   });
+  //   Router.events.on("routeChangeComplete", () => {
+  //     handleComplete();
+  //     NProgress.done();
+  //   });
+  //   Router.events.on("routeChangeError", () => {
+  //     handleComplete();
+  //     NProgress.done();
+  //   });
+  // }, []);
 
   return (
     <div className="bg-theme z-0">
