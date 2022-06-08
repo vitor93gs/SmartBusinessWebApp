6585defde6e0fc80317d46ac59c7543bd1a624d1
Vitor <vitor93gs@gmail.com>
2022-06-08 14:02:54 -0300

    corrigido import de useeffect na route smartacademy

--- src/pages/nossas-empresas/SMART%20ACADEMY.js
+++ src/pages/nossas-empresas/SMART%20ACADEMY.js
@@ -1,5 +1,5 @@
 import { useRouter } from "next/dist/client/router";
-import { useEffect } from "react/cjs/react.production.min";
+import { useEffect } from "react";
 
 export default function ProjetoCRD() {
   useEffect(() => {
