9d51b04f8bc865ecfb4bfd375823f675803341b2
Vitor <vitor93gs@gmail.com>
2022-06-14 10:35:22 -0300

    alinhado pc da duck com branch DEV, criados 3 botões novos

--- src/components/TabCarousel.js
+++ src/components/TabCarousel.js
@@ -1,141 +1,141 @@
-import { useState } from 'react';
-import { Transition } from '@headlessui/react';
-import { Carousel } from 'react-responsive-carousel';
+import { useState } from "react";
+import { Transition } from "@headlessui/react";
+import { Carousel } from "react-responsive-carousel";
 
-import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 
-import { Ebook } from './front-page-carousel/Ebook';
-import { FollowUs } from './front-page-carousel/FollowUs';
-import { Franqueado } from './front-page-carousel/Franqueado';
-import { FreeCourse } from './front-page-carousel/FreeCourse';
-import { News } from './front-page-carousel/News';
-import { SmartBusiness } from './front-page-carousel/SmartBusiness';
-import { SmartAcademy } from './front-page-carousel/SmartAcademy';
+import { Ebook } from "./front-page-carousel/Ebook";
+import { FollowUs } from "./front-page-carousel/FollowUs";
+import { Franqueado } from "./front-page-carousel/Franqueado";
+import { FreeCourse } from "./front-page-carousel/FreeCourse";
+import { News } from "./front-page-carousel/News";
+import { SmartBusiness } from "./front-page-carousel/SmartBusiness";
+import { SmartAcademy } from "./front-page-carousel/SmartAcademy";
 
 export default function Tabs2() {
-	const [selected, setSelected] = useState(0);
+  const [selected, setSelected] = useState(0);
 
-	return (
-		<Carousel
-			emulateTouch={true}
-			useKeyboardArrows={true}
-			showArrows={false}
-			showThumbs={false}
-			infiniteLoop={true}
-			onChange={(current) => setSelected(current)}
-		>
-			<div>
-				<div className="bg-bg1 h-screen w-screen bg-no-repeat bg-cover bg-center">
-					<Transition
-						show={selected === 0}
-						enter="transition-all ease-in duration-1000"
-						enterFrom="opacity-0"
-						enterTo="opacity-100"
-					>
-						<div className=" bg-fumaca1 h-screen w-screen bg-center">
-							<div className="sm:bg-elem1 bg-contain bg-no-repeat h-screen w-screen">
-								<Ebook />
-							</div>
-						</div>
-					</Transition>
-				</div>
-			</div>
-			<div>
-				<div className="bg-bg2 h-screen w-screen bg-no-repeat bg-cover bg-center">
-					<Transition
-						show={selected === 1}
-						enter="transition-all ease-in duration-1000"
-						enterFrom="opacity-0"
-						enterTo="opacity-100"
-					>
-						<div className=" absolute bg-fumaca2 h-screen w-screen bg-center">
-							<div className="sm:bg-elem2 bg-contain bg-no-repeat h-screen w-screen">
-								<Franqueado />
-							</div>
-						</div>
-					</Transition>
-				</div>
-			</div>
-			<div>
-				<div className="bg-bg3 h-screen w-screen bg-no-repeat bg-cover bg-center">
-					<Transition
-						show={selected === 2}
-						enter="transition-all ease-in duration-1000"
-						enterFrom="opacity-0"
-						enterTo="opacity-100"
-					>
-						<div className=" absolute bg-fumaca3 h-screen w-screen bg-center">
-							<div className="sm:bg-elem3 bg-contain bg-no-repeat h-screen w-screen">
-								<FreeCourse />
-							</div>
-						</div>
-					</Transition>
-				</div>
-			</div>
-			<div>
-				<div className="bg-bg4 h-screen w-screen bg-no-repeat bg-cover bg-center">
-					<Transition
-						show={selected === 3}
-						enter="transition-all ease-in duration-1000"
-						enterFrom="opacity-0"
-						enterTo="opacity-100"
-					>
-						<div className=" absolute bg-fumaca4 h-screen w-screen bg-center">
-							<div className="sm:bg-elem4 bg-contain bg-no-repeat h-screen w-screen">
-								<News />
-							</div>
-						</div>
-					</Transition>
-				</div>
-			</div>
-			<div>
-				<div className="bg-bg5 h-screen w-screen bg-no-repeat bg-cover bg-center">
-					<Transition
-						show={selected === 4}
-						enter="transition-all ease-in duration-1000"
-						enterFrom="opacity-0"
-						enterTo="opacity-100"
-					>
-						<div className=" absolute bg-fumaca5 h-screen w-screen bg-center">
-							<div className="sm:bg-elem5 bg-contain bg-no-repeat h-screen w-screen">
-								<SmartBusiness />
-							</div>
-						</div>
-					</Transition>
-				</div>
-			</div>
-			<div>
-				<div className="bg-bg6 h-screen w-screen bg-no-repeat bg-cover bg-center">
-					<Transition
-						show={selected === 5}
-						enter="transition-all ease-in duration-1000"
-						enterFrom="opacity-0"
-						enterTo="opacity-100"
-					>
-						<div className=" absolute bg-fumaca6 h-screen w-screen bg-center">
-							<div className="sm:bg-elem6 bg-contain bg-no-repeat h-screen w-screen">
-								<SmartAcademy />
-							</div>
-						</div>
-					</Transition>
-				</div>
-			</div>
-			<div>
-				<div className="bg-bg7 h-screen w-screen bg-no-repeat bg-cover bg-center">
-					<Transition
-						show={selected === 6}
-						enter="transition-all ease-in duration-1000"
-						enterFrom="opacity-0"
-						enterTo="opacity-100"
-					>
-						<div className=" absolute bg-fumaca7 h-screen w-screen bg-center">
-							<div className="sm:bg-elem7 bg-contain bg-no-repeat h-screen w-screen">
-								<FollowUs />
-							</div>
-						</div>
-					</Transition>
-				</div>
-			</div>
-		</Carousel>
-	);
+  return (
+    <Carousel
+      emulateTouch={true}
+      useKeyboardArrows={true}
+      showArrows={false}
+      showThumbs={false}
+      infiniteLoop={true}
+      onChange={(current) => setSelected(current)}
+    >
+      <div>
+        <div className="bg-bg1 h-screen w-screen bg-no-repeat bg-cover bg-center">
+          <Transition
+            show={selected === 0}
+            enter="transition-all ease-in duration-1000"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+          >
+            <div className=" bg-fumaca1 h-screen w-screen bg-center">
+              <div className="sm:bg-elem1 bg-cover bg-no-repeat h-screen w-screen">
+                <Ebook />
+              </div>
+            </div>
+          </Transition>
+        </div>
+      </div>
+      <div>
+        <div className="bg-bg2 h-screen w-screen bg-no-repeat bg-cover bg-center">
+          <Transition
+            show={selected === 1}
+            enter="transition-all ease-in duration-1000"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+          >
+            <div className=" absolute bg-fumaca2 h-screen w-screen bg-center">
+              <div className="sm:bg-elem2 bg-cover bg-no-repeat h-screen w-screen">
+                <Franqueado />
+              </div>
+            </div>
+          </Transition>
+        </div>
+      </div>
+      <div>
+        <div className="bg-bg3 h-screen w-screen bg-no-repeat bg-cover bg-center">
+          <Transition
+            show={selected === 2}
+            enter="transition-all ease-in duration-1000"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+          >
+            <div className=" absolute bg-fumaca3 h-screen w-screen bg-center">
+              <div className="bg-elem3 bg-cover bg-no-repeat sm:bg-center h-screen w-screen">
+                <FreeCourse />
+              </div>
+            </div>
+          </Transition>
+        </div>
+      </div>
+      <div>
+        <div className="bg-bg4 h-screen w-screen bg-no-repeat bg-cover bg-center">
+          <Transition
+            show={selected === 3}
+            enter="transition-all ease-in duration-1000"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+          >
+            <div className=" absolute bg-fumaca4 h-screen w-screen bg-center">
+              <div className="sm:bg-elem4 bg-cover bg-no-repeat h-screen w-screen">
+                <News />
+              </div>
+            </div>
+          </Transition>
+        </div>
+      </div>
+      <div>
+        <div className="bg-bg5 h-screen w-screen bg-no-repeat bg-cover bg-center">
+          <Transition
+            show={selected === 4}
+            enter="transition-all ease-in duration-1000"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+          >
+            <div className=" absolute bg-fumaca5 h-screen w-screen bg-center">
+              <div className="sm:bg-elem5 bg-cover bg-no-repeat h-screen w-screen">
+                <SmartBusiness />
+              </div>
+            </div>
+          </Transition>
+        </div>
+      </div>
+      <div>
+        <div className="bg-bg6 h-screen w-screen bg-no-repeat bg-cover bg-center">
+          <Transition
+            show={selected === 5}
+            enter="transition-all ease-in duration-1000"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+          >
+            <div className=" absolute bg-fumaca6 h-screen w-screen bg-center">
+              <div className="sm:bg-elem6 bg-cover bg-no-repeat h-screen w-screen">
+                <SmartAcademy />
+              </div>
+            </div>
+          </Transition>
+        </div>
+      </div>
+      <div>
+        <div className="bg-bg7 h-screen w-screen bg-no-repeat bg-cover bg-center">
+          <Transition
+            show={selected === 6}
+            enter="transition-all ease-in duration-1000"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+          >
+            <div className=" absolute bg-fumaca7 h-screen w-screen bg-center">
+              <div className="sm:bg-elem7 bg-cover bg-no-repeat h-screen w-screen">
+                <FollowUs />
+              </div>
+            </div>
+          </Transition>
+        </div>
+      </div>
+    </Carousel>
+  );
 }
--- src/components/front-page-carousel/News.js
+++ src/components/front-page-carousel/News.js
@@ -1,48 +1,42 @@
-import Image from 'next/dist/client/image';
-import logoBig from '../../assets/book_-_livro.png';
-import Typewriter from 'typewriter-effect';
-import Link from 'next/link';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import { useEffect } from 'react';
+import Image from "next/dist/client/image";
+import logoBig from "../../assets/book_-_livro.png";
+import Typewriter from "typewriter-effect";
+import Link from "next/link";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 export function News() {
-	useEffect(() => {
-		AOS.init();
-	}, []);
-	return (
-		<div className="bg-transparent flex justify-between items-center w-screen h-screen sm:px-36 px-7 py-7">
-			<div className="flex flex-col items-center sm:w-1/2 w-1/3 text-left  mr-auto">
-				<h1 className="text-gray1 sm:text-2xl text-base text-opacity-70 text-left sm:mb-12 h-36 w-full sm:ml-10">
-					<Typewriter
-						options={{
-							delay: 30,
-							cursor: '',
-							loop: false,
-						}}
-						onInit={(typewriter) => {
-							typewriter
-								.pauseFor(1000)
-								.typeString('FIQUE POR DENTRO DAS NOVIDADES!')
-								.start();
-						}}
-					/>
-				</h1>
-				<div data-aos="zoom-in" data-aos-duration="1500">
-					<Link href="">
-						<div className="div-neon opacity-60 rounded-sm text-center text-sm">
-							<span></span>
-							<span></span>
-							<span></span>
-							<span></span>
-							<button>SAIBA MAIS</button>
-						</div>
-					</Link>
-				</div>
-			</div>
-			<div className="w-1/2">
-				<Image src={logoBig} alt="logoBig" width={400} height={540} />
-			</div>
-		</div>
-	);
+  useEffect(() => {
+    AOS.init();
+  }, []);
+  return (
+    <div className="bg-transparent flex justify-between items-center w-screen h-screen sm:px-36 px-7 py-7">
+      <div className="flex flex-col items-center sm:w-1/2 w-1/3 text-left  mr-auto">
+        <h1 className="text-gray1 sm:text-2xl text-base text-opacity-70 text-left sm:mb-12 h-36 w-full sm:ml-10">
+          <Typewriter
+            options={{
+              delay: 30,
+              cursor: "",
+              loop: false,
+            }}
+            onInit={(typewriter) => {
+              typewriter
+                .pauseFor(1000)
+                .typeString("FIQUE POR DENTRO DAS NOVIDADES!")
+                .start();
+            }}
+          />
+        </h1>
+        <div data-aos="zoom-in" data-aos-duration="1500">
+          <Link href="">
+            <button className="btn-4">SAIBA MAIS</button>
+          </Link>
+        </div>
+      </div>
+      <div className="w-1/2">
+        <Image src={logoBig} alt="logoBig" width={400} height={540} />
+      </div>
+    </div>
+  );
 }
