af3f7bc3dd3c9c8b5b1f75e7fe41aabeb1eb4ab5
Vitor Santos <vitor93gs@gmail.com>
2022-06-21 15:25:04 -0300

    criado estilo de letras das tabs com linha lateral e fundo colorido e adicionado nas paginas com o lado quebrado

--- src/components/front-page-carousel/FreeCourse.js
+++ src/components/front-page-carousel/FreeCourse.js
@@ -3,6 +3,7 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect, useState } from "react";
 import { Transition } from "@headlessui/react";
+import Typewriter from "typewriter-effect";
 
 export function FreeCourse() {
   const [showLuz, setShowLuz] = useState(false);
@@ -30,7 +31,7 @@ export function FreeCourse() {
           <div className="absolute visible bottom-0 left-0 bg-no-repeat h-screen w-screen bg-contain bg-center pointer-events-none max-w-tela bg-fumaca2 " />
         </Transition>
       </div>
-      <div className="flex flex-col items-center w-1/2 text-left  mr-auto">
+      {/* <div className="flex flex-col items-center w-1/2 text-left  mr-auto">
         <h1
           data-aos="zoom-in"
           data-aos-duration="2500"
@@ -40,6 +41,60 @@ export function FreeCourse() {
         </h1>
 
         <div className="ml-10" data-aos="fade-up" data-aos-duration="3000">
+          <Link href="">
+            <button className="btn-1">SAIBA MAIS</button>
+          </Link>
+        </div>
+      </div> */}
+      <div className="w-1/2 relative mr-1/6">
+        <h1 className="text-white2 text-right text-4xl h-12">
+          <Typewriter
+            options={{
+              delay: 30,
+              cursor: "",
+              loop: false,
+            }}
+            onInit={(typewriter) => {
+              typewriter
+                .pauseFor(1000)
+                .typeString("<strong>SMART BUSINESS!</strong>")
+                .start();
+            }}
+          />
+        </h1>
+        <div className="h-40 text-4xl font-thin tracking-wider text-white2 text-right after:-right-0.5 after:absolute after:w-0.5 after:bg-golden after:bg-opacity-100 after:h-44 after:mt-12 after:top-0">
+          <h1 className="pr-3 mb-2">AS CRIPTOMOEDAS SÃO</h1>
+          <h1 className="text-preto mb-2">
+            <span className="bg-golden pl-1 pr-3 font-medium">
+              O FUTURO DO DINHEIRO,
+            </span>
+          </h1>
+          <h1 className="pr-3 mb-2">
+            PREPARE-SE PARA A <br />
+            ERA DIGITAL!
+          </h1>
+        </div>
+        <h1 className="text-gray1 w-full text-right text-lg mb-5 mt-5 font-thin h-14">
+          <Typewriter
+            options={{
+              delay: 30,
+              cursor: "",
+              loop: false,
+            }}
+            onInit={(typewriter) => {
+              typewriter
+                .pauseFor(1800)
+                .typeString("APRENDA AGORA CONCEITOS E<br/>")
+                .typeString("CURIOSIDADES DA CRIPTOECONOMIA")
+                .start();
+            }}
+          />
+        </h1>
+        <div
+          className="absolute right-0"
+          data-aos="fade-up"
+          data-aos-duration="3000"
+        >
           <Link href="">
             <button className="btn-1">SAIBA MAIS</button>
           </Link>
